4532bbe643737b179fadaf23d6e5cdc2b2b5cf3c
= <=>
2022-10-05 10:20:55 -0400

    i

--- src/app/service/animal.service.ts
+++ src/app/service/animal.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Animal } from '../model/animal';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnimalService {
+  listeAnimaux: Array<Animal>;
+  unAnimal: Animal;
 
-  constructor() { }
+  apiGestionAnimaux = 'http://localhost:8080/api/api_gestionAnimaux.php';
+
+  constructor(public httpClient: HttpClient) {
+    this.listeAnimaux = new Array<Animal>();
+    this.unAnimal = new Animal(0, '', 0, 0, new Date());
+  }
+
+  obtenirAnimauxDuClient(idClient: number) {
+    return this.httpClient.get(this.apiGestionAnimaux + '?action=obtenirAnimauxDuClient&idClient=' + idClient);
+  }
 }
